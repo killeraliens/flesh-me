a6703a2b92d229d52ccfdc560ac8f3f16a96b3de
killeraliens <killeraliens@outlook.com>
2019-06-12 09:37:10 -0700

    inputs can toggle with aria pressed

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 'use strict';
 
-
-// console.log(headImg["src"]);
-
 function changeSrc(el, src, alt) {
 
-  // const applyAlt = event.currentTarget['alt'];
-  // el['alt'] = applyAlt;
-  // console.log($(`#${el}`).attr('alt'));
+  if ( $(`#${el}`).attr('src') === src ) {
+    $(`#${el}`).removeAttr('src');
+    $(`#${el}`).removeAttr('alt');
+    $(`#${el}`).attr('aria-pressed', 'false');
+  } else {
+    $(`#${el}`).attr('aria-pressed', 'true');
+    $(`#${el}`).fadeOut(0, function() {
+      $(`#${el}`).attr('src',src);
+      $(`#${el}`).attr('alt', alt);
+      console.log($(`#${el}`).attr('alt'));
+      $(`#${el}`).on('load', function(){
+        $(`#${el}`).fadeTo(1000,.9, 'swing');
+      });
+    });
 
-    $(`#${el}`)
-        .fadeOut(200, function() {
-            $(`#${el}`).attr('src',src);
-            $(`#${el}`).attr('alt', alt);
-          console.log($(`#${el}`).attr('alt'));
-            $(`#${el}`).on('load', function(){
-                $(`#${el}`).fadeTo(1000,.9, 'swing');
-            });
-        });
+  }
+  console.log($(`#${el}`).attr('aria-pressed'));
 
 }
