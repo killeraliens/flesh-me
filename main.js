5cc517801aba03049dd1d7923983778e851694e3
killeraliens <killeraliens@outlook.com>
2019-06-12 08:42:11 -0700

    alt text applied to layers

--- main.js
+++ main.js
@@ -3,11 +3,17 @@
 
 // console.log(headImg["src"]);
 
-function changeSrc(el, src) {
+function changeSrc(el, src, alt) {
+
+  // const applyAlt = event.currentTarget['alt'];
+  // el['alt'] = applyAlt;
+  // console.log($(`#${el}`).attr('alt'));
 
     $(`#${el}`)
         .fadeOut(200, function() {
             $(`#${el}`).attr('src',src);
+            $(`#${el}`).attr('alt', alt);
+          console.log($(`#${el}`).attr('alt'));
             $(`#${el}`).on('load', function(){
                 $(`#${el}`).fadeTo(1000,.9, 'swing');
             });
