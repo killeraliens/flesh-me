d78aa0b4515c02b29f2cb21e9da9e5a0e9972a25
killeraliens <killeraliens@outlook.com>
2019-06-12 10:01:21 -0700

    text bubble changes with selections

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 function changeSrc(el, src, alt) {
 
+  const bubbleText = $('#js-bubble-text');
+
   if ( $(`#${el}`).attr('src') === src ) {
     $(`#${el}`).removeAttr('src');
     $(`#${el}`).removeAttr('alt');
     $(`#${el}`).attr('aria-pressed', 'false');
+    bubbleText.text('FLESH ME').removeClass('shrink-text');
   } else {
     $(`#${el}`).attr('aria-pressed', 'true');
     $(`#${el}`).fadeOut(0, function() {
@@ -15,6 +18,8 @@ function changeSrc(el, src, alt) {
       $(`#${el}`).on('load', function(){
         $(`#${el}`).fadeTo(1000,.9, 'swing');
       });
+      const newTxt = $(`#${el}`).attr('alt');
+      bubbleText.text(`I have ${newTxt}`).addClass('shrink-text');
     });
 
   }
