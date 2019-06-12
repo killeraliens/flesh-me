96c6557498f24eec03e2dbab3ef3c95b523eee44
killeraliens <killeraliens@outlook.com>
2019-06-12 15:38:33 -0700

    cleanup

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function changeSrc(el, src, alt) {
   const bubbleText = $('#js-bubble-text');
 
   if ( $(`#${el}`).attr('src') === src ) {
-    $(`#${el}`).removeAttr('src');
-    $(`#${el}`).removeAttr('alt');
+    $(`#${el}`).removeAttr('src alt');
+    // $(`#${el}`).removeAttr('alt');
     $(`#${el}`).attr('aria-pressed', 'false');
     bubbleText.text('FLESH ME').removeClass('shrink-text');
   } else {
@@ -16,7 +16,7 @@ function changeSrc(el, src, alt) {
       $(`#${el}`).attr('alt', alt);
       console.log($(`#${el}`).attr('alt'));
       $(`#${el}`).on('load', function(){
-        $(`#${el}`).fadeTo(1000,.9, 'swing');
+        $(`#${el}`).fadeTo(800,.9, 'swing');
       });
       const newTxt = $(`#${el}`).attr('alt');
       bubbleText.text(`I have ${newTxt}`).addClass('shrink-text');
